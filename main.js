bdb12c85707a6b9cebbe6624383d00ec3e128fca
Maryam Selim <40206568+MaryamTSelim@users.noreply.github.com>
2018-12-20 19:16:15 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 window.addEventListener("load", function (e) {
     let video = document.getElementById("hajiVideo");
     let isPlaying = false;
+    let isfullScreen = false;
     video.addEventListener("click", function (e) {
-        console.log(video)
         if (!isPlaying) {
             video.play();
             isPlaying = true;
         } else {
-
             video.pause();
             isPlaying = false;
+        }
+    }, false);
+    video.addEventListener("dblclick", function (e) {
+        if (!isfullScreen) {
+            video.requestFullscreen();
+            isfullScreen = true;
+        } else {
+            document.exitFullscreen()
+            isfullScreen = false;
         }
     }, false);
 })
